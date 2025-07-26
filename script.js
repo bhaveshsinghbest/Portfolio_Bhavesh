3d0d674bfb824515b692bdccd0c037f93b41a0e2
bhaveshsinghbest <bhaveshsinghbest111@gmail.com>
2025-07-26 19:30:01 +0530

    project responsive updated

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  const carousel = document.querySelector('#projectCarousel');
+  const bsCarousel = new bootstrap.Carousel(carousel, {
+    interval: 32000, // in milliseconds
+    ride: 'carousel'
+  });
+
